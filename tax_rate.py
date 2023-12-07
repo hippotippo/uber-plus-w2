4a7d0e020d6feb1cde3bf0212aab9e5ba53a3f7b
hippotippo <147953756+hippotippo@users.noreply.github.com>
2023-12-07 14:41:15 -0500

    Update tax_rate.py

--- tax_rate.py
+++ tax_rate.py
@@ -1,4 +1,6 @@
-def calculate_tax(filing_status, taxable_income):
+def calculate_tax(filing_status, taxable_income, total_deductions, child_credits):
+    taxable_income -= total_deductions
+    taxable_income = max(0, taxable_income)
     if filing_status == 1:  # Single
         return calculate_single_tax(taxable_income)
     elif filing_status == 2:  # Head of Household
@@ -74,6 +76,23 @@ def calculate_married_filing_separately_tax(income):
     else:
         return 93300.75 + (income - 346875) * 0.37
 
+def get_standard_deduction(filing_status):
+    if filing_status == 1:  # Single
+        return 13850
+    elif filing_status == 2:  # Head of Household
+        return 20800
+    elif filing_status == 3:  # Married Filing Jointly
+        return 27700
+    elif filing_status == 4:  # Married Filing Separately
+        return 13850
+
+def get_child_tax_credit(number_of_children, number_of_teens, filing_status):
+    credit_per_child = 2000
+    credit_per_teen = 1600
+    income_limit = 400000 if filing_status == 3 else 200000
+    # Modify this as per the actual income limits and calculation rules
+    return (number_of_children * credit_per_child) + (number_of_teens * credit_per_teen)
+
 def get_total_income_from_w2s(number_of_w2s):
     total_income = 0
     for i in range(number_of_w2s):
@@ -81,7 +100,12 @@ def get_total_income_from_w2s(number_of_w2s):
         total_income += income
     return total_income
 
-# Main function to prompt user input
+def calculate_side_hustle_tax(net_income, filing_status):
+    # This function calculates the tax on side hustle income based on the filing status and net income
+    # Assuming the side hustle income is added on top of other income
+    return calculate_tax(filing_status, net_income, 0, 0)  # No additional deductions or credits for this part
+
+
 def main():
     print("Enter your filing status:")
     print("1: Single")
@@ -93,12 +117,46 @@ def main():
     number_of_w2s = int(input("How many W-2 forms do you need to enter? "))
     income_from_w2s = get_total_income_from_w2s(number_of_w2s)
 
-    side_hustle_income = float(input("Enter your total income from side hustles: "))
-    total_income = income_from_w2s + side_hustle_income
+    itemize = input("Do you plan to itemize deductions? (yes/no): ").lower().strip() == "yes"
+    total_deductions = 0
+    if itemize:
+        total_deductions = float(input("Enter the total of your itemized deductions: "))
+    else:
+        total_deductions = get_standard_deduction(filing_status)
+    
+       # Child tax credits
+    number_of_children = int(input("Enter the number of qualifying children under 16: "))
+    number_of_teens = int(input("Enter the number of qualifying children between 16 and 23: "))
+    child_credits = get_child_tax_credit(number_of_children, number_of_teens, filing_status)
 
-    tax_owed = calculate_tax(filing_status, total_income)
+    side_hustle_gross_income = float(input("Enter your total gross income from side hustles: "))
+    side_hustle_expenses = float(input("Enter your total expenses from side hustles: "))
+    side_hustle_net_income = side_hustle_gross_income - side_hustle_expenses
+    total_income = income_from_w2s + side_hustle_net_income
+
+    # Calculate the total tax with and without the side hustle income
+    total_tax_with_side_hustle = calculate_tax(filing_status, total_income, total_deductions, child_credits)
+    total_tax_without_side_hustle = calculate_tax(filing_status, income_from_w2s, total_deductions, child_credits)
+
+    # The side hustle tax is the difference between these two amounts
+    side_hustle_tax = total_tax_with_side_hustle - total_tax_without_side_hustle
+
+
+    
+
+ 
+
+    tax_owed = calculate_tax(filing_status, total_income, total_deductions, child_credits)
     print(f"Total taxable income: {total_income}")
-    print(f"Tax owed for filing status {filing_status} with a taxable income of {total_income}: {tax_owed}")
+    print(f"Total deductions: {total_deductions}")
+    print(f"Total child tax credits: {child_credits}")
+    print(f"Tax owed after deductions and credits: {tax_owed}")
+    print(f"Side hustle tax: {side_hustle_tax}")
+    quarterly_taxes_2024 = side_hustle_tax / 4
+    print(f"2024 Quarterly Taxes (estimated at 8%): {quarterly_taxes_2024}")
+    if side_hustle_tax > 1000:
+        print("Immediate penalty applicable if above $1000 for the year. Estimated quarterly payments should be submitted.")
 
 
-main()
+if __name__ == "__main__":
+    main()
